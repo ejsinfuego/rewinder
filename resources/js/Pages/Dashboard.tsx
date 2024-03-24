d56dab4c634273d6d64aa2bcc0632e243daff0f5
Ej <edzeljohnsinfuego@gmail.com>
2024-03-24 22:15:45 +0800

    config: switch to typescript and initialize ant design

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,7 +1,8 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
-
+import { DatePicker } from 'antd';
+import GenerateForm from '@/Components/GenerateForm/GenerateForm';
 export default function Dashboard({ auth }: PageProps) {
     return (
         <AuthenticatedLayout
@@ -9,14 +10,10 @@ export default function Dashboard({ auth }: PageProps) {
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
         >
             <Head title="Dashboard" />
+        <div className="flex justify-center mt-6 max-w-full border border-2">
+            <GenerateForm />
+        </div>
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
-                    </div>
-                </div>
-            </div>
         </AuthenticatedLayout>
     );
 }
